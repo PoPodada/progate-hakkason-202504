0389dac556ce715e875401a2b771d199ad0e7df7
massan02 <tankful-secure0c@icloud.com>
2025-04-23 17:10:52 +0900

    refactor:  biome checkを通過できるように修正 Firebase接続テスト関数をuseCallbackでメモ化し、forEachの代わりにfor...ofを使用するように変更

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,28 @@
 import { collection, getDocs } from "firebase/firestore";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { db } from "./firebase";
 
 function App() {
-	// Firebase接続テスト関数なのだ！
-	const testFirebaseConnection = async () => {
+	// Firebase接続テスト関数をuseCallbackでメモ化するのだ！
+	const testFirebaseConnection = useCallback(async () => {
 		try {
 			console.log("Firebaseに接続を試みるのだ...");
 			const querySnapshot = await getDocs(collection(db, "test-collection"));
 			console.log("Firebase接続成功なのだ！🎉");
-			querySnapshot.forEach((doc) => {
+
+			// forEachの代わりにfor...ofを使うのだ！パフォーマンスが良くなるのだ🍵
+			for (const doc of querySnapshot.docs) {
 				console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-			});
+			}
 		} catch (error) {
 			console.error("Firebase接続エラーなのだ...😭", error);
 		}
-	};
+	}, []); // 依存配列は空なのだ
 
 	// コンポーネントがマウントされたときに接続テストを実行するのだ
 	useEffect(() => {
 		testFirebaseConnection();
-	}, []);
+	}, [testFirebaseConnection]); // 依存配列にtestFirebaseConnectionを追加するのだ！
 
 	return <div className="font-bold">test</div>;
 }
