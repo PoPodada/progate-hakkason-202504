c59d15697e217ae78af21727904e69ae4d7b0d6a
PoPodada <goito.722@icloud.com>
2025-04-26 13:33:10 +0900

    共同編集を実装

--- client/src/features/Edit/components/editor.tsx
+++ client/src/features/Edit/components/editor.tsx
@@ -6,38 +6,75 @@ import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import RichEditorToolbar from "./rich-editor-toolbar";
 import "@/styles/editor.scss";
+import * as Y from 'yjs'
+import { WebsocketProvider } from "y-websocket";
+import Collaboration from "@tiptap/extension-collaboration";
+import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
+import { useEffect } from "react";
+
+const ydoc = new Y.Doc()
+
+const provider = new WebsocketProvider(
+    "ws://127.0.0.1:1234",
+    "1",
+    ydoc
+)
 
 const Editor = () => {
-    const editor = useEditor({
-        extensions: [ StarterKit,
-            TaskItem.configure({
-              nested: true,
-            }),
-            TaskList,
-            Link.configure({
-              openOnClick: true,
-            }),
-            Placeholder.configure({
-              placeholder: "Write something …",
-            }),],
-        content: "<p>Hello World! 🌍️</p>",
-        editorProps: {
-            attributes: {
-                class: "prose prose-base m-5 focus:outline-none"
-            }
-        }
-    })
-    if (!editor) {
-        return null
-    }
-    return (
-        <div className="w-2/3 mt-10 mx-auto border-gray-500 border-2">
-            <RichEditorToolbar editor={editor} />
-            <div className="p-3 overflow-y-scroll h-[500px] overflow-hidden mt-3" >
-                <EditorContent editor={editor} />
-            </div>
-        </div>
-    )
-}
+    console.log(provider)
+    
 
-export default Editor
+	const editor = useEditor({
+		extensions: [
+			StarterKit.configure({
+                history: false,
+            }),
+			TaskItem.configure({
+				nested: true,
+			}),
+			TaskList,
+			Link.configure({
+				openOnClick: true,
+			}),
+			Placeholder.configure({
+				placeholder: "Write something …",
+			}),
+            Collaboration.configure({document: ydoc}),
+            CollaborationCursor.configure({
+                provider,
+                user: {
+                  name: "Cyandi Laupter",
+                  color: "#ffcc00",
+                },
+              }),
+		],
+		content: "<p>Hello World! 🌍️</p>",
+		editorProps: {
+			attributes: {
+				class: "prose prose-base m-5 focus:outline-none",
+			},
+		},
+	});
+
+    useEffect(() => {
+        if (provider.shouldConnect) {
+            provider.disconnect()
+          } else {
+            provider.connect()
+          }
+    },[])
+
+	if (!editor) {
+		return null;
+	}
+	return (
+		<div className="w-2/3 mt-10 mx-auto border-gray-500 border-2">
+			<RichEditorToolbar editor={editor} />
+			<div className="p-3 overflow-y-scroll h-[500px] overflow-hidden mt-3">
+				<EditorContent editor={editor} />
+			</div>
+		</div>
+	);
+};
+
+export default Editor;
